ffe09ac2f32c0e17afc26746c8fef2f8b6e29e3b
Meme <myom142@hotmail.com>
2023-07-20 00:31:56 +0300

    update

--- js/Signup_Validation.js
+++ js/Signup_Validation.js
@@ -1,6 +1,8 @@
 const validation = new JustValidate("#signup");
 
 validation
+
+//Name Validation:
     .addField("#firstName", [
         {
             rule: "required"
@@ -13,15 +15,17 @@ validation
         }
     ])
 
+// Email Validation:
     .addField("#email", [
         {rule: "required"}, {rule: "email"}
 
     ])
 
+// Password Validation:
     .addField("#password", [
         {rule: "required"}, {rule: "password"}
     ])
-    // password confirmation
+// Password Confirmation Validation:
     .addField("#password2", [
         {
             validator: (value, fields) => {
@@ -31,4 +35,27 @@ validation
         }
     ])
 
-   
+    // Usertype Validation:
+    .addField("#usertype", [
+      {rule: "required"}
+    
+   ])
+
+// Phone Validation:
+    function validateNumber(input) {
+      var re = /^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/
+
+      return re.test(input)
+    }
+
+    function validateForm(event) {
+      var number = document.getElementById('phone').value
+      if (!validateNumber(number)) {
+        const ele = document.getElementById('result')
+        ele.innerHTML = 'يرجى ادخال رقم هاتف صحيح'
+        ele.style.color = 'red'
+       }
+      event.preventDefault()
+    }
+
+    document.getElementById('signup').addEventListener('submit', validateForm);
